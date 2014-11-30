e22caf76a90718ca1ec1fd123019853679b2b8c9
eeue56 <eep41b@bangor.ac.uk>
2014-11-30 02:43:12 +0000

    added some basics

---
+++ basics/EmptyExectuableClass.java
@@ -0,0 +1,15 @@
+// Some variation on 
+
+public class EmptyExectuableClass {
+
+    // public static or static public
+    // String[] or args[] or String []
+    //
+    // Style - public static void in that order
+    // Brackets should be with String
+    // args could be anything else, but args is 
+    // most common
+    public static void main(String[] args){
+
+    }
+}
---
+++ basics/questions.md
@@ -0,0 +1,10 @@
+Write a minimal class that can be run by calling
+    javac <filename>.java
+    java <filename>
+
+Write a class that, when run, prints "Hello World"
+    > Expected output : "Hello World"
+
+Write a class that, when run, prints the sum of 1..100
+    > Expected output : 5050
+    
